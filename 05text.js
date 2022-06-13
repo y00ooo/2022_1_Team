bdc230a2a2a0354f148656b214b9f7a0b0624633
y00ooo <77039683+y00ooo@users.noreply.github.com>
2022-06-13 10:13:44 +0900

    Create 05text.js

---
+++ 05text.js
@@ -0,0 +1,553 @@
+let texts_1 = [
+  { text: "......", click_reaction: "next", text_who: "나" },
+  { text: "......어?", click_reaction: "next", text_who: "나" },
+  { text: "여긴 어디지?", click_reaction: "next", text_who: "나" },
+  {
+    text: "나 또 잠들었나? 분명 정문기입 팀플 하고 있었는데.",
+    click_reaction: "next",
+    text_who: "나",
+  },
+  { text: "......", click_reaction: "next", text_who: "나" },
+  { text: "아 잠만", click_reaction: "next", text_who: "나" },
+  {
+    text: "빨리 일어나야 해... 나 p5js 저장 안 했단 말이야!!!!",
+    click_reaction: "textoff",
+    text_who: "나",
+  },
+];
+
+let texts_5 = [
+  {
+    text: "...뭐야? 내가 지금 이러고 있을 때가 아닌데.",
+    click_reaction: "next",
+    text_who: "나",
+  },
+  {
+    text: "대체 어떻게 해야 꿈에서 깰 수 있지?",
+    click_reaction: "next",
+    text_who: "나",
+  },
+  {
+    text: "[왠지 이 책을 만져봐야 할 것 같은 기분이 든다.]",
+    click_reaction: "textoff",
+    text_who: "나",
+  },
+  {
+    text: "......잠깐",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "이게 뭐야~!!!!!!!!!!!!!!!!!!!!!",
+    click_reaction: "textoff",
+    text_who: "말레피센트",
+  },
+];
+
+let texts_7 = [
+  {
+    text: "뭐야… 여긴 또 어디야?",
+    click_reaction: "crowon",
+    text_who: "말레피센트",
+  },
+  {
+    text: "말레피센트, 큰일이야!",
+    click_reaction: "next",
+    text_who: "???",
+  },
+  {
+    text: "잠시만요. 저는 말레피센트가 아닌데요.",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "지금 공주는 저주로 잠들어 있고, 세상 사람들은 모두 너를 의심하고 있지.",
+    click_reaction: "next",
+    text_who: "???",
+  },
+  {
+    text: "아니 저는 저주 거는 법을 모르는데요...! 그나저나 누구세요?",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "나? 디아블로잖아! 아아, 말레피센트! 지금 그게 중요한 게 아냐.",
+    click_reaction: "next",
+    text_who: "디아블로",
+  },
+   {
+    text: "왕자가 너를 쫓고 있어. 이대로 붙잡힌다면 너는 죽음에 이르고 말 거야.",
+    click_reaction: "next",
+    text_who: "디아블로",
+  },
+  {
+    text: "저... 저 아직 정문기입 팀플 완성 못했는데요. 벌써 죽으면 안 되는데...",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "물론 방법은 있어. 네가 먼저 성으로 가서 공주를 깨우고 모두의 오해를 풀면 돼!",
+    click_reaction: "next",
+    text_who: "디아블로",
+  },
+    {
+    text: "하지만 지금 성문을 여는 열쇠는 세 개로 조각난 상태야. 너는 총 세 번의 관문을 통과해야 한다는 뜻이지.",
+    click_reaction: "next",
+    text_who: "디아블로",
+  },
+  {
+    text: "일단 해보겠습니다...",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "좋아. 그럼 우선 지도를 확인해보겠어?",
+    click_reaction: "next",
+    text_who: "디아블로",
+  },
+  {
+    text: "보라색으로 표시된 곳부터 바로 이동할 수 있을 거야.",
+    click_reaction: "textoff",
+    text_who: "디아블로",
+  },
+];
+let texts_12 = [
+  {
+    text: "드디어 성 앞에 도착했다.",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "지금까지 모은 열쇠 조각을 합쳐서 성문을 열 수 있는 열쇠로 만들자.",
+    click_reaction: "textoff",
+    text_who: "말레피센트",
+  },
+];
+
+let texts_14 = [
+  {
+    text: "좋아, 이걸로 문을 열 수 있겠어.",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "어서 공주의 방으로 가자.",
+    click_reaction: "textoff",
+    text_who: "말레피센트",
+  },
+  0,
+];
+
+let texts_15 = [
+  {
+    text: "저 사람이 바로 오로라 공주군. 당장 깨우자!",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "...",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "잠깐, 그런데 어떻게 깨우지? 난 왕자도 아니잖아!",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "...",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "그, 그렇다면 내가 키스를…",
+    click_reaction: "crowon",
+    text_who: "말레피센트",
+  },
+  {
+    text: "잠깐 멈춰 말레피센트!!!!!!",
+    click_reaction: "next",
+    text_who: "디아블로",
+  },
+  {
+    text: "악 깜짝이야!!! 놀랐잖아요!!!",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "휴… 늦지 않아서 다행이군. 그나저나 여기까지 무사히 도착하다니, 역시 널 믿고 있었어.",
+    click_reaction: "next",
+    text_who: "디아블로",
+  },
+  {
+    text: "감사합니다. 그런데 잠시만요. 제가 지금 공주한테 키스를 하러 가야 해서요.",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "말레피센트, 기다려. 키스만으로 공주를 깨울 수 있다는 건 편견이야. 왜 이래? 이게 무슨 동화책도 아니고!",
+    click_reaction: "next",
+    text_who: "디아블로",
+  },
+  {
+    text: "(동화책보다 더 심한 상황인 것 같지만 조용히 하기로 하자.)",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "잘 생각해봐. 네가 제일 잘하는 마법 있잖아, 그걸 써!",
+    click_reaction: "next",
+    text_who: "디아블로",
+  },
+  {
+    text: "그게 뭐죠...?",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "뭐겠어? 박수잖아, 네 주특기! ",
+    click_reaction: "next",
+    text_who: "디아블로",
+  },
+  {
+    text: "빨리 가서 박수 쳐!!",
+    click_reaction: "next",
+    text_who: "디아블로",
+  },
+  {
+    text: "(무슨 마녀 주특기가 박수 치기야?!!!)",
+    click_reaction: "textoff",
+    text_who: "말레피센트",
+  },
+];
+
+let texts_16 = [
+  {
+    text: "공주가 눈을 뜬 것 같다. 말을 걸어보자.",
+    click_reaction: "textoff",
+    text_who: "말레피센트",
+  },
+  0,
+];
+let texts_17 = [
+  {
+    text: "그… 공주님, 정신이 드시나요?",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "우선 오해하실 상황이라는 걸 아는데요. 맹세코 제가 저주를 건 게 아닙니다. 저는 집에서 얌전하게 코딩하던 대학생일 뿐이에요.",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "어머, 말레피센트! 오랜만이에요! 이게 얼마만이죠?",
+    click_reaction: "next",
+    text_who: "오로라",
+  },
+  {
+    text: "...?",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "어렸을 적 저를 돌봐주셔서 정말 감사했어요. 꼭 감사 인사를 드리고 싶었는데 이렇게 만나네요!",
+    click_reaction: "next",
+    text_who: "오로라",
+  },
+  {
+    text: "예...?",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "...? 말레피센트, 혹시 무슨 문제라도?",
+    click_reaction: "next",
+    text_who: "오로라",
+  },
+  {
+    text: "크, 크흠!!!",
+    click_reaction: "next",
+    text_who: "디아블로",
+  },
+  {
+    text: "디아블로, 이게 어떻게 된 거죠?",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "으음... 분명 이 이야기가 맞는데... 뭔가 착각했나...",
+    click_reaction: "next",
+    text_who: "디아블로",
+  },
+  {
+    text: "뭔가 착오가 있었나봐요. 어, 어쨌든 잘 된 거 아닌가요? 하하! 그쪽도 재밌었죠?",
+    click_reaction: "next",
+    text_who: "디아블로",
+  },
+  {
+    text: "갑자기 존댓말을 쓰시네요.",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "크흠… 미안합니다. 이 세계에서는 저주 그런 거 없고, 모두가 행복하게 살아가고 있어요. 보아하니 공주도 당신을 엄청 따르는 것 같군요.",
+    click_reaction: "next",
+    text_who: "디아블로",
+  },
+  {
+    text: "제가 다른 이야기와 헷갈린 것 같네요! 헤헷! ...죄송합니다.",
+    click_reaction: "next",
+    text_who: "디아블로",
+  },
+  {
+    text: "제가 뭘 어쩌겠어요. 괜찮습니다. 그나저나 저는 이제 어떻게 되는 건가요?",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "아, 그래! 그걸로 할 말이 있어요. 제가 큰 실수를 했으니 대신 아주 좋은 기회를 드릴게요.",
+    click_reaction: "next",
+    text_who: "디아블로",
+  },
+  {
+    text: "그게 뭐죠?",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "바로...",
+    click_reaction: "next",
+    text_who: "디아블로",
+  },
+  {
+    text: "선택권입니다!!!",
+    click_reaction: "next",
+    text_who: "디아블로",
+  },
+  {
+    text: "당신은 이 곳에 남아서 말레피센트로 살아갈 건지, 아니면 현실 세계로 돌아갈 건지 고를 수 있어요.",
+    click_reaction: "next",
+    text_who: "디아블로",
+  },
+  {
+    text: "자, 어떻게 하시겠어요?",
+    click_reaction: "textoff",
+    text_who: "디아블로",
+  },
+  0,
+];
+
+let texts_18 = [
+  {
+    text: "그렇다면 원래 있던 곳으로 바로 보내드릴게요.",
+    click_reaction: "next",
+    text_who: "디아블로",
+  },
+  {
+    text: "그래도 당신을 좋아하는 공주에게 인사 한 번은 부탁드려요!",
+    click_reaction: "next",
+    text_who: "디아블로",
+  },
+  {
+    text: "어어... 그래요. 잘 있으세요, 공주님!",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "다음에 또 만나요!",
+    click_reaction: "textoff",
+    text_who: "말레피센트",
+  },
+];
+let texts_19 = [
+  {
+    text: "이 책, 뭔가 익숙한데.",
+    click_reaction: "next",
+    text_who: "나",
+  },
+  {
+    text: "…왠지 만져봐야 할 것 같은 기분이 들어, 또.",
+    click_reaction: "textoff",
+    text_who: "나",
+  },
+  0,
+];
+let texts_23 = [
+  {
+    text: "역시 이 세계가 재밌죠? 공주님도 기뻐하실 거예요.",
+    click_reaction: "next",
+    text_who: "디아블로",
+  },
+  {
+    text: "그래도 언제든지 원하신다면 원래 세계로 돌아갈 수 있게 해드릴게요!",
+    click_reaction: "textoff",
+    text_who: "디아블로",
+  },
+  0,
+];
+let texts_24 = [
+  {
+    text: "벌써 5년이라는 시간이 흘렀군.",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "나 때문에 저주에 걸린 줄 알았던 공주랑, 나를 죽이려고 쫓아오던 왕자가 결혼하는 모습까지 지켜보다니...",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "역시 오래 살고 볼 일이야~",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "앗, 시작한다.",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "신랑, 신부는 서로를 영원히 사랑할 것을 맹세합니까?",
+    click_reaction: "princeon",
+    text_who: "육종환",
+  },
+  {
+    text: "네, 맹세합니다.",
+    click_reaction: "princesson",
+    text_who: "왕자",
+  },
+  {
+    text: "네, 맹세합니다!",
+    click_reaction: "next",
+    text_who: "오로라",
+  },
+    {
+    text: "호우~~ 잘 어울린다~~",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+    {
+    text: "(그나저나 5년이면 정말 시간이 많이 흘렀지. 그동안 참 재밌었지만...)",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+    {
+    text: "내가 이곳에 남으려 했던 이유는 오로라와 함께하기 위함이었잖아. 이제 오로라에게는 왕자가 있으니, 더이상 여기 있을 이유가 없네.",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+   {
+    text: "디아블로! 언제든 돌아가게 해 준다는 약속, 아직 유효하죠?",
+    click_reaction: "crowon",
+    text_who: "말레피센트",
+  },
+  {
+    text: "물론이에요. 말레피센트, 재밌었어요?",
+    click_reaction: "next",
+    text_who: "디아블로",
+  },
+  {
+    text: "예. 엄청요.",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "그치만 이제 돌아갈 시간이 된 것 같아요.",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+    {
+    text: "저도 졸업은 해야죠.",
+    click_reaction: "next",
+    text_who: "말레피센트",
+  },
+  {
+    text: "하하하. 그래도 하나만 기억해요. 말레피센트, 오로라는 분명 당신 덕에 행복한 시간을 보냈을 겁니다.",
+    click_reaction: "next",
+    text_who: "디아블로",
+  },
+  {
+    text: "우리 이야기의 한 조각이 되어 줘서 고마웠어요.",
+    click_reaction: "next",
+    text_who: "디아블로",
+  },
+   {
+    text: "자, 이제 돌아갈까요?",
+    click_reaction: "textoff",
+    text_who: "디아블로",
+  },
+  0,
+];
+// 이 양식대로 맞춰서 하면 됨
+// let texts_n = [
+// {
+//     text : "",
+//     click_reaction : "",
+//     text_who: ""
+// },
+// ]
+
+let texts = [
+  0,
+  texts_1,
+  0,
+  0,
+  0,
+  texts_5,
+  0,
+  texts_7,
+  0,
+  0,
+  0,
+  0,
+  texts_12,
+  0,
+  texts_14,
+  texts_15,
+  texts_16,
+  texts_17,
+  texts_18,
+  texts_19,
+  0,
+  0,
+  0,
+  texts_23,
+  texts_24,
+];
+
+// game intro text box
+let gi_texts_0 = [
+  "가시덤불이 성으로 가는 길을 막고 있다.\n마우스 위치를 따라 손이 화면에 나타나면,\n직접 손을 움직여서 가시덤불을 걷어내자.",
+  "이걸 치우지 않으면 앞으로 갈 수 없겠어…",
+];
+let gi_texts_1 = [
+  "다리가 무너져 앞으로 갈 수 없다.\n키보드 방향키를 조작해 돌덩이를 모으자.\n꼬리에 닿거나 화면 밖으로 나가면 모은 돌을 잃게 되니 주의하자.",
+  "이 다리를 건너야 성으로 갈 수 있는데...",
+];
+let gi_texts_2 = [
+  "배고픈 용이 성문을 지키고 있다.\n메뉴판에서 음식을 클릭해 용에게 갖다 주자.\n용이 주는 힌트를 참고해 알맞은 음식을 찾아야 한다.",
+  "이 성문 너머에 오로라 공주가 있어.",
+];
+
+let gi_texts = [gi_texts_0, gi_texts_1, gi_texts_2];
+
+// game outro text box
+let go_texts_0 = [
+  "무사히 가시덤불을 걷어냈다.\n첫 번째 열쇠조각을 클릭해 다음 스테이지로 이동하자.",
+];
+let go_texts_1 = [
+  "다리를 수리하는데 필요한 돌을 충분히 모았다.\n두 번째 열쇠조각을 클릭해 다음 스테이지로 이동하자.",
+];
+let go_texts_2 = [
+  "배부른 용은 이제 나를 신경도 쓰지 않는다.\n마지막 열쇠 조각을 클릭해 다음 스테이지로 이동하자.",
+];
+let go_texts = [go_texts_0, go_texts_1, go_texts_2];
+
+// dragon text box
+let dragon_texts = [
+  "난 빨리 완성되는 음식이 좋아.",
+  "시원한 음식이 필요해!!",
+  "달콤한 음식이 먹고 싶어~",
+  "배부르다! 넌 착한 마녀구나!",
+];
