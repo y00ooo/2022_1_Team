b6dc3dfb5809da1ba1e4c5f39a1b99e31cbd1127
y00ooo <77039683+y00ooo@users.noreply.github.com>
2022-06-16 09:48:45 +0900

    Update 03variable.js

--- 03variable.js
+++ 03variable.js
@@ -1,8 +1,20 @@
-let scene = 8;
+let scene = 0;
 //fonts
 let font1;
 // video
 let key_video;
+// songs
+let titlesong;
+// let dreamsound0;
+// let dreamsound1;
+// let dreamsound2;
+// let booksound;
+let openingsong;
+let gamesong;
+let endingsong;
+let endingsong2;
+
+let micOn = false;
 
 // scene 0
 
@@ -15,6 +27,8 @@ let textboxOn = true;
 let text_index = 0;
 
 let crowOn = false;
+let crowovercheck = true;
+let crowclicked = false;
 
 // scene 2
 let bI_2 = new backgroundImage();
@@ -36,6 +50,7 @@ let scene4_last_time = 0;
 // scene 5
 let scene5_now_time = 0;
 let scene5_delta_time = 0;
+let scene5_end_time = 0;
 let scene5_book_clicked = 0;
 let bookOn = true;
 let book = new Book();
@@ -54,20 +69,26 @@ let LeyeX = 0;
 let LeyeY = 0;
 
 // scene 6
+let scene6_end_time = 0;
 let bI_6 = new backgroundImage();
+let scene6_now_time = 0;
 // scene 7
+let scene7_now_time = 0;
 let bI_7 = new backgroundImage();
 let crow = new Crow();
 
 // scene 8
 let map_images = [];
-let worldmap = new Map();
+let map_over_images = [];
+
 let stage = 0;
 let mark1 = new Mark(0);
 let mark2 = new Mark(1);
 let mark3 = new Mark(2);
 let mark4 = new Mark(3);
+let worldmap = new Map();
 
+let key_over_images = [];
 let key_images = [];
 let key1 = new Key(0);
 let key2 = new Key(1);
@@ -96,14 +117,16 @@ let GOTB_0 = new gameoutroTextBox(0, 1600, 1000);
 let bI_10 = new backgroundImage();
 let GITB_1 = new gameintroTextBox(1, 1600, 1000);
 // snake game
-let snake_speed = 6;
+let snake_speed = 9;
 let snake_done = false;
+
+let gridSize = 80;
 
 let numSegments = 10;
 let direction = "right";
-let xStart = 100;
-let yStart = 500;
-let diff = 50;
+let xStart = gridSize * 2;
+let yStart = gridSize * 5;
+let diff = gridSize;
 let xCor = [];
 let yCor = [];
 let xFruit = 0;
@@ -141,19 +164,25 @@ let key_video_startTime = 9999999999999;
 let scene12_Timenow = 0;
 // scene 13
 let bI_13 = new backgroundImage();
+
 // scene 14
+let scene14_now_time = 0;
+let scene14_last_time = 0;
 let bI_14 = new backgroundImage();
 let lock = new Lock();
 let keycursor = new keyCursor();
 let scene14_clicked = 0;
 
 // scene 15
+let scene15_now_time = 0;
 let bI_15 = new backgroundImage();
 
 // scene 16
+let clapOn = true;
 let clap = new Clap();
+
 let bI_16 = new backgroundImage();
-let mic_height = 0;
+let mic_w = 0;
 let mic;
 let mic_overed = 0;
 
@@ -162,6 +191,7 @@ let sleepprincess_images = [];
 let sleepprincess = new sleepPrincess();
 
 // scene 17
+
 let bI_17 = new backgroundImage();
 let princessOn = true;
 let princess = new Princess();
@@ -171,8 +201,12 @@ let selectbox0 = new selectBox(0);
 let selectbox1 = new selectBox(1);
 
 // scene 18
+let scene18_end_time = 0;
+let scene18_now_time = 0;
 let bI_18 = new backgroundImage();
 // scene 19
+let scene19_now_time = 0;
+let ending_book_clicked_time = 0;
 let ending_book = new EndingBook();
 
 // scene 20
@@ -181,9 +215,13 @@ let bI_20 = new backgroundImage();
 let bI_21 = new backgroundImage();
 // scene 22
 let bI_22 = new backgroundImage();
-
+let scene23_now_time = 0;
+// let scene23_end_time = 0;
 // scene 24
+let scene24_now_time = 0;
 let bI_24 = new backgroundImage();
 let wedprincess = new Princess();
+let sixOn = false;
+let six = new Six();
 let princeOn = false;
 let prince = new Prince();
